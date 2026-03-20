b016c2f47af347a4c81ccc9e39f853470781c168
jjunseo02 <kavin0916@naver.com>
2026-03-20 19:01:09 +0900

    test1

---
+++ pypractice/c2p02.py
@@ -0,0 +1,19 @@
+# 변수의 자료형 확인하기
+
+"""
+아래 변수와 자료형 함께 출력하기 !!!!!!!!!!!type(변수) 입력해야됨!!!!!!!!!!!!!
+a = "파이썬"
+b = 2026
+c = 3.14
+d = false
+"""
+
+a = "파이썬"
+b = 2026
+c = 3.14
+d = False
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
